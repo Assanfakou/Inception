715009de09dffc74b40957a496837290d4721a9d
Assanfakou <fakouhasan@gmail.com>
2026-05-04 13:01:44 -0400

    taking shouts

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Setup directories and permissions
+# Derectorys for socket and change there ownership to mysql
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-# Initialize fresh database
+# Initialize new database
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing MariaDB..."
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 fi
 
-# Start MariaDB temporarily for setup
+# Start MariaDB int the baground for setup and stop it later by
+# 	it's process id
 mysqld --user=mysql &
 pid="$!"
 
-# Wait until ready
+# whaiting fo the database to get readdy 
 until mysqladmin ping --silent 2>/dev/null; do
     echo "Waiting for MariaDB..."
     sleep 1
 done
 
-# Create database and user
+# Create database and users and set there password
 mysql <<EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
@@ -30,6 +31,8 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-# Restart as final process
+# kill the setup process that was running 
 kill $pid && wait $pid
+
+#execute the mysql to be started as a container
 exec mysqld --user=mysql
